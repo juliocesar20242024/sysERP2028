846df5946cf5b7a33052ba0b476edd835d02ef23
eccefiles <eccefiles@hotmail.com>
2025-06-25 15:05:59 -0700

    init work to angular page

---
+++ src/app/core/layout/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.html',
+  standalone: true,
+  styleUrl: './sidebar.css'
+})
+export class Sidebar {
+
+}
---
+++ src/app/core/layout/topbar/topbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [],
+  templateUrl: './topbar.html',
+  standalone: true,
+  styleUrl: './topbar.css'
+})
+export class Topbar {
+
+}
